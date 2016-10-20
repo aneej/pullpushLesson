410329ad9149e4d04915a23f7705dd4c2859bcc1
Aneej Matthai <AneejM@gmail.com>
2016-10-20 16:53:13 +0530

    initial version

---
+++ Person.java
@@ -0,0 +1,13 @@
+public class Person {
+	
+	/**
+	**  Any  role in CRM project would need to be an  instance  of class Person
+	**/
+
+	// define attributes & methods here
+	String name ;
+	
+	public String getName() {
+		return  name ;
+	}
+}
